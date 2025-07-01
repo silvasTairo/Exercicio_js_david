e841a34bf09f92331f527f3323dd122208abde81
TairoSilva <162700891+silvasTairo@users.noreply.github.com>
2025-07-01 08:07:58 -0300

    Update exercicios.js

--- exercicios.js
+++ exercicios.js
@@ -14,34 +14,3 @@ console.log(contarElementos(animais));
 
 
 // Exercício 2
-
-function separa(nums) {
-    let totalPares = 0;
-    let totalImpares = 0;
-    
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 === 0) {
-            totalPares++;
-        } else {
-            totalImpares++;
-        }
-    }
-
-const pares = new Array(totalPares);
-const impares = new Array(totalImpares);
-  
-let indicePar = 0;
-let indiceImpar = 0;
-    
-for (let i = 0; i < nums.length; i++) {
-  if (nums[i] % 2 === 0) {
-    pares[indicePar] = nums[i];
-            indicePar++;
-} else {
-      impares[indiceImpar] = nums[i];
-            indiceImpar++;
-      }
-}
-
-    return [pares, impares];
-}
